752d1bcb045c79239e7c6d4d59ba2df5504f1623
Marglodis Tovar <marglodistovar@gmail.com>
2025-03-02 19:45:29 -0300

    crear modelo y schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,15 @@
 import { Module } from '@nestjs/common';
-import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './auth/auth.module';
+
 
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_URI ?? ''),
     AuthModule,
-    MongooseModule.forRoot('mongodb://localhost:27017')
     ],
   controllers: [],
   providers: [],
